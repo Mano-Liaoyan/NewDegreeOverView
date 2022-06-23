68757a881f48eab3b821d21f52d6c32e85f872b2
Mano-Liaoyan <ben.jaminlyyf21@gmail.com>
2022-06-24 00:01:29 +0800

    Test Git

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home () {
   return (
     <div className={styles.container}>
       <Head>
@@ -17,7 +17,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing!{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
